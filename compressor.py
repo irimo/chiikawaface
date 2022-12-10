5bdc566023ff0853398e10c2ec6827ff14b820e0
Moe Irikida <irimodi@gmail.com>
2022-12-10 23:02:18 +0900

    ここまで戻すと大丈夫

--- compressor.py
+++ compressor.py
@@ -7,47 +7,50 @@ import copy
 import os
 import random
 
-class compressor:
-    def parts_recognize(self):
-        # XML_PATH_FACE = "/usr/local/lib/python3.7/site-packages/cv2/data/haarcascade_frontalface_default.xml"
-        # XML_PATH_LEFTEYE = "/usr/local/lib/python3.7/site-packages/cv2/data/haarcascade_lefteye_2splits.xml"
-        # XML_PATH_RIGHTEYE = "/usr/local/lib/python3.7/site-packages/cv2/data/haarcascade_righteye_2splits.xml"
 
-        print("parts_recognize() start")
-        law_path = "./images/lenna.png"
-        img_origin = cv2.imread(law_path)
-        # imgheight = img_origin.shape[0]
-        # imgwidth = img_origin.shape[1]
-        img_gray = cv2.cvtColor(img_origin, cv2.COLOR_BGR2GRAY)
-        # INPUT_IMG_PATH = "./images/lenna.png"
-        OUTPUT_IMG_PATH = "./out/lenna.png"
-        
-        
-        # 輪郭
-        # classifier = cv2.CascadeClassifier(XML_PATH_FACE)
-        # targets = classifier.detectMultiScale(img_gray)
-        # targets = self.face_reco(img_gray)
-        # for x, y, w, h in targets:
-        #     cv2.rectangle(img_gray, (x, y), (x + w, y + h), (255, 0, 0), 2)
+def parts_recognize():
+    XML_PATH_FACE = "/usr/local/lib/python3.7/site-packages/cv2/data/haarcascade_frontalface_default.xml"
+    XML_PATH_LEFTEYE = "/usr/local/lib/python3.7/site-packages/cv2/data/haarcascade_lefteye_2splits.xml"
+    XML_PATH_RIGHTEYE = "/usr/local/lib/python3.7/site-packages/cv2/data/haarcascade_righteye_2splits.xml"
 
-        # 左目
-        # classifier = cv2.CascadeClassifier(XML_PATH_LEFTEYE)
-        # targets = classifier.detectMultiScale(img_gray)
-        # for x, y, w, h in targets:
-        #     cv2.rectangle(img_gray, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    print("parts_recognize() start")
+    # print(cv2.__file__)
+    law_path = "./images/lenna.png"
+    img_origin = cv2.imread(law_path)
+    # imgheight = img_origin.shape[0]
+    # imgwidth = img_origin.shape[1]
+    img_gray = cv2.cvtColor(img_origin, cv2.COLOR_BGR2GRAY)
+    INPUT_IMG_PATH = "./images/lenna.png"
+    OUTPUT_IMG_PATH = "./out/lenna.png"
+    
+    
+    # img = cv2.imread(INPUT_IMG_PATH)
+    img_gray = cv2.cvtColor(img_origin, cv2.COLOR_BGR2GRAY)
+    
+    # 輪郭
+    classifier = cv2.CascadeClassifier(XML_PATH_FACE)
+    targets = classifier.detectMultiScale(img_gray)
+    for x, y, w, h in targets:
+        cv2.rectangle(img_gray, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-        # # 右目
-        # classifier = cv2.CascadeClassifier(XML_PATH_RIGHTEYE)
-        # targets = classifier.detectMultiScale(img_gray)
-        # for x, y, w, h in targets:
-        #     cv2.rectangle(img_gray, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        
-        cv2.imwrite(OUTPUT_IMG_PATH, img_gray)
+    # 左目
+    # classifier = cv2.CascadeClassifier(XML_PATH_LEFTEYE)
+    # targets = classifier.detectMultiScale(img_gray)
+    # for x, y, w, h in targets:
+    #     cv2.rectangle(img_gray, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-    # def face_reco(self, img_gray):
-    #     XML_PATH_FACE = "/usr/local/lib/python3.7/site-packages/cv2/data/haarcascade_frontalface_default.xml"
-    #     classifier = cv2.CascadeClassifier(XML_PATH_FACE)
-    #     targets = classifier.detectMultiScale(img_gray)
-    #     return targets
-# cprs = compressor()   
-# cprs.parts_recognize()
+    # # 右目
+    # classifier = cv2.CascadeClassifier(XML_PATH_RIGHTEYE)
+    # targets = classifier.detectMultiScale(img_gray)
+    # for x, y, w, h in targets:
+    #     cv2.rectangle(img_gray, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    
+    cv2.imwrite(OUTPUT_IMG_PATH, img_gray)
+
+def face_reco(img_gray):
+    XML_PATH_FACE = "/usr/local/lib/python3.7/site-packages/cv2/data/haarcascade_frontalface_default.xml"
+
+    classifier = cv2.CascadeClassifier(XML_PATH_FACE)
+    targets = classifier.detectMultiScale(img_gray)
+    
+parts_recognize()

ad0a459ae10d7db99e0bbcb3c9aef35772593baf
Moe Irikida <irimodi@gmail.com>
2022-12-10 23:25:48 +0900

    変数名を小文字に

--- compressor.py
+++ compressor.py
@@ -9,32 +9,29 @@ import random
 
 class compressor:
     def parts_recognize(self):
-        XML_PATH_FACE = "/usr/local/lib/python3.7/site-packages/cv2/data/haarcascade_frontalface_default.xml"
-        XML_PATH_LEFTEYE = "/usr/local/lib/python3.7/site-packages/cv2/data/haarcascade_lefteye_2splits.xml"
-        XML_PATH_RIGHTEYE = "/usr/local/lib/python3.7/site-packages/cv2/data/haarcascade_righteye_2splits.xml"
-
         print("parts_recognize() start")
-        # print(cv2.__file__)
+        xml_face = "/usr/local/lib/python3.7/site-packages/cv2/data/haarcascade_frontalface_default.xml"
+        xml_lefteye = "/usr/local/lib/python3.7/site-packages/cv2/data/haarcascade_lefteye_2splits.xml"
+        xml_righteye = "/usr/local/lib/python3.7/site-packages/cv2/data/haarcascade_righteye_2splits.xml"
+
         law_path = "./images/lenna.png"
         img_origin = cv2.imread(law_path)
         # imgheight = img_origin.shape[0]
         # imgwidth = img_origin.shape[1]
         img_gray = cv2.cvtColor(img_origin, cv2.COLOR_BGR2GRAY)
-        INPUT_IMG_PATH = "./images/lenna.png"
-        OUTPUT_IMG_PATH = "./out/lenna.png"
+        input_img_path = "./images/lenna.png"
+        output_img_path = "./out/lenna.png"
         
-        
-        # img = cv2.imread(INPUT_IMG_PATH)
         img_gray = cv2.cvtColor(img_origin, cv2.COLOR_BGR2GRAY)
         
         # 輪郭
-        classifier = cv2.CascadeClassifier(XML_PATH_FACE)
+        classifier = cv2.CascadeClassifier(xml_face)
         targets = classifier.detectMultiScale(img_gray)
         for x, y, w, h in targets:
             cv2.rectangle(img_gray, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
         # 左目
-        # classifier = cv2.CascadeClassifier(XML_PATH_LEFTEYE)
+        # classifier = cv2.CascadeClassifier(xml_lefteye)
         # targets = classifier.detectMultiScale(img_gray)
         # for x, y, w, h in targets:
         #     cv2.rectangle(img_gray, (x, y), (x + w, y + h), (255, 0, 0), 2)
@@ -45,7 +42,7 @@ class compressor:
         # for x, y, w, h in targets:
         #     cv2.rectangle(img_gray, (x, y), (x + w, y + h), (255, 0, 0), 2)
         
-        cv2.imwrite(OUTPUT_IMG_PATH, img_gray)
+        cv2.imwrite(output_img_path, img_gray)
 
     def face_reco(self, img_gray, classifier):
         # XML_PATH_FACE = "/usr/local/lib/python3.7/site-packages/cv2/data/haarcascade_frontalface_default.xml"

e7bb0d4ca4d534196cec4cc33ba75cf8c2ed2de3
Moe Irikida <sakurapancakeore@gmail.com>
2022-12-13 21:12:40 +0900

    スーパークラス？作った　動く

--- compressor.py
+++ compressor.py
@@ -16,18 +16,16 @@ class compressor:
 
         law_path = "./images/lenna.png"
         img_origin = cv2.imread(law_path)
-        # imgheight = img_origin.shape[0]
-        # imgwidth = img_origin.shape[1]
-        print("parts_recognize() start")
-        img_gray = cv2.cvtColor(img_origin, cv2.COLOR_BGR2GRAY)
-        # input_img_path = "./images/lenna.png"
         output_img_path = "./out/lenna.png"
         
         img_gray = cv2.cvtColor(img_origin, cv2.COLOR_BGR2GRAY)
         
         # 輪郭
-        classifier = cv2.CascadeClassifier(self.xml_face)
-        targets = classifier.detectMultiScale(img_gray)
+        face = square_reco(img_gray, self.xml_face)
+        targets = face.reco()
+        # targets = self.face_reco(img_gray, self.xml_face)
+        # classifier = cv2.CascadeClassifier(self.xml_face)
+        # targets = classifier.detectMultiScale(img_gray)
         for x, y, w, h in targets:
             cv2.rectangle(img_gray, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
@@ -45,12 +43,17 @@ class compressor:
         
         cv2.imwrite(output_img_path, img_gray)
 
-    def face_reco(self, img_gray, classifier):
-        # XML_PATH_FACE = "/usr/local/lib/python3.7/site-packages/cv2/data/haarcascade_frontalface_default.xml"
 
-        # classifier = cv2.CascadeClassifier(XML_PATH_FACE)
-        # del XML_PATH_FACE
-        targets = classifier.detectMultiScale(img_gray)
-        
+class square_reco:
+    img_gray = ""
+    xml_path = ""
+    def __init__(self, img_gray, xml_path):
+        self.img_gray = img_gray
+        self.xml_path = xml_path
+    def reco(self):
+        classifier = cv2.CascadeClassifier(self.xml_path)
+        targets = classifier.detectMultiScale(self.img_gray)
+        return targets
+    
 compr = compressor()
 compr.parts_recognize()

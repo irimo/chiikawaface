20948430ec84f770b8c0c6f4a8b89b4a4d700eda
Moe Irikida <sakurapancakeore@gmail.com>
2022-12-13 21:20:32 +0900

    輪郭、左目（鼻になっちゃった）動いた

--- compressor.py
+++ compressor.py
@@ -21,15 +21,18 @@ class compressor:
         img_gray = cv2.cvtColor(img_origin, cv2.COLOR_BGR2GRAY)
         
         # 輪郭
-        face = square_reco(img_gray, self.xml_face)
+        face = face_reco(img_gray)
         targets = face.reco()
-        # targets = self.face_reco(img_gray, self.xml_face)
-        # classifier = cv2.CascadeClassifier(self.xml_face)
-        # targets = classifier.detectMultiScale(img_gray)
         for x, y, w, h in targets:
             cv2.rectangle(img_gray, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
         # 左目
+        lefteye = lefteye_reco(img_gray)
+        targets = lefteye.reco()
+        for x, y, w, h in targets:
+            cv2.rectangle(img_gray, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+
         # classifier = cv2.CascadeClassifier(xml_lefteye)
         # targets = classifier.detectMultiScale(img_gray)
         # for x, y, w, h in targets:
@@ -47,13 +50,32 @@ class compressor:
 class square_reco:
     img_gray = ""
     xml_path = ""
-    def __init__(self, img_gray, xml_path):
+    xml_base_dir = "/usr/local/lib/python3.7/site-packages/cv2/data/"
+    def __init__(self, img_gray, xml_filename):
         self.img_gray = img_gray
-        self.xml_path = xml_path
+        self.xml_path = self.xml_base_dir + xml_filename
     def reco(self):
         classifier = cv2.CascadeClassifier(self.xml_path)
         targets = classifier.detectMultiScale(self.img_gray)
         return targets
-    
+
+class face_reco(square_reco):
+    xml_face = "haarcascade_frontalface_default.xml"
+    def __init__(self, img_gray):
+        print("face_reco init")
+        super().__init__(img_gray, self.xml_face)
+
+class lefteye_reco(square_reco):
+    xml_lefteye = "haarcascade_lefteye_2splits.xml"
+    def __init__(self, img_gray):
+        print("lefteye_reco init")
+        super().__init__(img_gray, self.xml_lefteye)
+
+class righteye_reco(square_reco):
+    xml_lefteye = "haarcascade_lefteye_2splits.xml"
+    def __init__(self, img_gray):
+        print("lefteye_reco init")
+        super().__init__(img_gray, self.xml_lefteye)
+        
 compr = compressor()
 compr.parts_recognize()
